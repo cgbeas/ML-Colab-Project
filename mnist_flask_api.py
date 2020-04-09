fcc4566f12343ef88207c8b740286867e4607710
Ubuntu <ubuntu@ip-172-31-9-70.us-west-1.compute.internal>
2020-04-09 03:40:55 +0000

    mnist debug

--- mnist_flask_api.py
+++ mnist_flask_api.py
@@ -1,12 +1,13 @@
 from keras.models import load_model
-from PIL import Image
+from keras.preprocessing import image
 import numpy as np
 from flasgger import Swagger
 from flask import Flask, request
+import tensorflow as tf
+
 app = Flask(__name__)
 swagger = Swagger(app)
 
-model = load_model('./model.h5')
 
 
 @app.route('/predict_digit', methods=['POST'])
@@ -19,10 +20,23 @@ def predict_digit():
           type: file
           required: true
     """
-    im = Image.open(request.files['image'])
-    im2arr = np.array(im).reshape((1, 1, 28, 28))
-    return str(np.argmax(model.predict(im2arr)))
+    # Important to use same graph used by previous model
+    global model
+    model = load_model('./model.h5')
+    global graph
+    graph = tf.compat.v1.get_default_graph() 
+
+    # image.load_img allows to rescale image on load.
+    img = image.load_img(request.files['image'], target_size=(28, 28), color_mode = 'grayscale')
+    x = image.img_to_array(img).reshape((1,28,28))
+    # Need to use this function because model.prdedict expects a batch of samples instead of just 1
+    x = np.expand_dims(x, axis=0)
+
+    with graph.as_default():
+        pred = model.predict(x)[0]
+    return str(np.argmax(pred))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5050, debug=True)
+    #app.run()
